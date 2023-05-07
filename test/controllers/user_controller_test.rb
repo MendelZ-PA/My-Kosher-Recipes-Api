c60d9a1fc08748e2d8b8a6fb06e8a5aef55c370a
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-05-07 17:18:50 -0400

    renamed all controller files in plural form

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { name: "Mussia", email: "mussiaz@test.com", password: "password", password_confirmation: "password" }
+      assert_response 201
+    end
+  end
 end
