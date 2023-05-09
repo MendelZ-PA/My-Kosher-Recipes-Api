a54cefa0d7ef595046f2196bb25bc48bcabe0576
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-05-08 21:57:52 -0400

    adds routes for creating ingredients, recipes, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   post "users" => "users#create"
 
   post "sessions" => "sessions#create"
+
+  post "ingredients" => "ingredients#create"
+
+  post "recipes" => "recipes#create"
+
+  post "pantry_items" => "pantry_items#create"
+
+  post "kosher_substitutes" => "kosher_substitutes#create"
 end
