f1c4b778ea065e2127aee07baf14f555053484b4
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-05-10 20:50:20 -0400

    adds rack-cors gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  POST "/users" => "users#create"
-  GET "/users" => "users#index"
-  GET "/users/:id" => "users#show"
+  post "/users" => "users#create"
+  get "/users" => "users#index"
+  get "/users/:id" => "users#show"
 
-  POST "/sessions" => "sessions#create"
-  GET "/sessions" => "session#index"
-  GET "/sessions/:id" => "sessions#show"
+  post "/sessions" => "sessions#create"
+  get "/sessions" => "session#index"
+  get "/sessions/:id" => "sessions#show"
 
-  POST "/ingredients" => "ingredients#create"
-  GET "/ingredients" => "ingredients#index"
-  GET "/ingredients/:id" => "ingredients#show"
+  post "/ingredients" => "ingredients#create"
+  get "/ingredients" => "ingredients#index"
+  get "/ingredients/:id" => "ingredients#show"
 
-  POST "/recipes" => "recipes#create"
-  GET "/recipes" => "recipes#index"
-  GET "/recipes/:id" => "recipes#show"
+  post "/recipes" => "recipes#create"
+  get "/recipes" => "recipes#index"
+  get "/recipes/:id" => "recipes#show"
 
-  POST "/pantry_items" => "pantry_items#create"
-  GET "/pantry_items" => "pantry_items#index"
-  GET "pantry_items/:id" => "pantry_items#show"
+  post "/pantry_items" => "pantry_items#create"
+  get "/pantry_items" => "pantry_items#index"
+  get "pantry_items/:id" => "pantry_items#show"
 
-  POST "/kosher_substitutes" => "kosher_substitutes#create"
-  GET "/kosher_substitutes" => "kosher_substitutes#index"
-  GET "/kosher_substitutes/:id" => "kosher_substitutes#show"
+  post "/kosher_substitutes" => "kosher_substitutes#create"
+  get "/kosher_substitutes" => "kosher_substitutes#index"
+  get "/kosher_substitutes/:id" => "kosher_substitutes#show"
 end
