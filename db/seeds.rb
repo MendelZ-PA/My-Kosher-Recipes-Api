241101c9ad7decd6a3c9d09f8c0766be2acf0aa4
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-05-14 17:23:14 -0400

    added data via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Ingredient.create(
+  [{ measurement: "3 Cans",
+     name: "Tuna",
+     recipe_id: 4,
+     is_kosher: true }]
+)
+
+KosherSubstitute.create(
+  [{ name: "Tuna",
+     ingredient_id: 7,
+     brand: "NO SPECIFIC BRAND",
+     url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fjpg.houseofkosher.com%2Fcontent%2Fimages%2Fthumbs%2F0139740_raskins-chunk-light-tuna-in-water.jpeg&tbnid=vyoSR2rdl-hRWM&vet=12ahUKEwir3bSbvvX-AhX7GFkFHVlvDZYQMygAegQIARA9..i&imgrefurl=https%3A%2F%2Fwww.houseofkosher.com%2F%23!%2Fhb%2Fc%2F387-0%2Fc%2F388-387%2Fc%2F524-388%2Fm%2F000000%2Fr%2F99401%2Fhe%2FPhiladelphia%2Fkosher-tuna%2Fkosher-canned-food%2Fkosher-grocery%2F&docid=QlVKnlTxR1YabM&w=1000&h=1000&q=raskin%27s%20tuna%20can&hl=en&ved=2ahUKEwir3bSbvvX-AhX7GFkFHVlvDZYQMygAegQIARA9" }]
+)
